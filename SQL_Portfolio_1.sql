736350f0d4b7a6278c73508d648018093ee19263
AtticusPaulsen <93317927+AtticusPaulsen@users.noreply.github.com>
2021-10-29 07:32:13 +1000

    Update SQL_Portfolio_1.sql

--- SQL_Portfolio_1.sql
+++ SQL_Portfolio_1.sql
@@ -1,26 +1,36 @@
 -- In the below examples my Database is called 'DotaHeroPlayer' and there are two tables titled 'Player_Hero_Performance' and 'Hero Performance'
 -- These were imported from csv files: https://www.datdota.com/players/hero-combos?default=true and https://www.datdota.com/heroes/performances?default=true
 -- This was built in Azure Data Studio, Below are samples of functions in SQL on this data
+-- This SQL Portfolio is located at https://github.com/AtticusPaulsen/SQL_Portfolio/blob/main/SQL_Portfolio_1.sql
 
 
---QUERIES
+--                                                                                  QUERIES
 --Show total game count for all instances of heros in player hero combos in descending order
-
-SELECT PHP.Hero, SUM(PHP.total_count) AS 'Total Game Count'
+SELECT PHP.Hero, SUM(PHP.total_count) AS Total_game_Count, CONVERT(Decimal (10,2), AVG(PHP.Kills)) AS Kills
 FROM DotaHeroPlayer.dbo.player_Hero_Performance AS PHP
 GROUP BY PHP.Hero
-ORDER BY 'Total Game Count' DESC
+ORDER BY Kills DESC
+
+-- Help FY pick the best players for his team, he wants an Axe and Queen of Pain on his team or KuroKy
+SELECT Player, Winrate, Hero
+FROM dbo.player_Hero_Performance
+WHERE Hero IN (SELECT Hero FROM dbo.player_Hero_Performance WHERE Hero='Queen of Pain' OR Hero='Axe') AND Player<>'Fy' AND Winrate>'60.00' OR Player='Kuroky'
+ORDER BY Winrate DESC
+
+-- Biggest hero earners in game with a confidence requirement of above 30 games recorded
+SELECT Hero, Wins, Losses, Winrate, GPM, GS 
+FROM dbo.Hero_Performance 
+WHERE Total_Count >30
+ORDER BY GPM DESC
 
 -- Show all games where the player FY has a winrate of above 55% 
-
 SELECT * 
 FROM dbo.player_Hero_Performance 
 WHERE Player='Fy' AND Winrate >'55.00'
 
 
--- VIEWS
+--                                                                                     VIEWS
 -- View Creation for sum of total game counts from player hero combos
-
 CREATE view View1 AS
 SELECT PHP.Hero, SUM(PHP.total_count) AS 'TGC'
 FROM DotaHeroPlayer.dbo.player_Hero_Performance AS PHP
@@ -28,7 +38,6 @@ GROUP BY PHP.Hero
 
 
 -- Conditional View Creation 
-
 CREATE View best_heroes AS 
 SELECT Hero, Winrate 
 FROM dbo.player_Hero_Performance 
@@ -36,20 +45,32 @@ WHERE winrate >'60.00'
 
 
 -- Replace View and add new column and AND statement
-
 ALTER View best_heroes AS 
 SELECT Hero, Winrate, kills 
 FROM Dbo.dbo.player_Hero_Performance 
 WHERE winrate >'60.00' AND kills>'1'
 
 
---JOINS
+--                                                                                     JOINS
 ---- Shows Total View Count aggregated from Player Hero Combos inner joined with total game count from hero performances in Descending order on hero performances
-
 SELECT View1.Hero, View1.TGC, HP.Total_count 
 FROM View1
 INNER JOIN DotaHeroPlayer.dbo.Hero_Performance AS HP ON HP.Hero = View1.Hero
 WHERE HP.Total_Count >5
 ORDER BY HP.Total_count DESC
 
+--                                                                                    TEMP TABLES
+---- Shows Total View Count aggregated from Player Hero Combos inner joined with total game count from hero performances in Descending order on hero performances
+DROP Table if exists #TempTable1#
+Create Table #TempTable1#
+(
+Finalist_Possible_Teammates NVARCHAR(300),
+Finalist_Hero NVARCHAR(300)
+)
 
+Insert into #TempTable1#
+SELECT Player AS Finalist_Possible_Teammates, Hero AS Finalist_Hero
+FROM dbo.player_Hero_Performance
+WHERE Hero IN (SELECT Hero FROM dbo.player_Hero_Performance WHERE Hero='Queen of Pain' OR Hero='Axe') AND Player<>'Fy' AND Winrate>'60.00' OR Player='Kuroky'
+
+Select * From #TempTable1#
